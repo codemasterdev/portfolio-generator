438ffb312ae5ae4c3ac3fda942758898f6e0ef2d
codemasterdev <skillz1029@gmail.com>
2021-09-14 07:06:16 -0400

    inquirer question prompt set up

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
     }
+  ])
+  .then(answers => console.log(answers));
 
-    console.log('================');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
-printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err)
+//     throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
